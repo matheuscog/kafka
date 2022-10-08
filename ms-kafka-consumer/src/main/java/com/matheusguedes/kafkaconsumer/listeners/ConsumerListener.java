b75705c176754e45d4c35dca21a3ce7a36cf3477
Matheus Guedes <matheus.cog@hotmail.com>
2022-10-08 17:07:01 -0300

    Acréscimo de record interceptor

--- ms-kafka-consumer/src/main/java/com/matheusguedes/kafkaconsumer/listeners/ConsumerListener.java
+++ ms-kafka-consumer/src/main/java/com/matheusguedes/kafkaconsumer/listeners/ConsumerListener.java
@@ -1,6 +1,7 @@
 package com.matheusguedes.kafkaconsumer.listeners;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Log4j2
@@ -12,19 +13,14 @@ public class ConsumerListener {
         log.info("listenerOneGroupOne ::: Nova mensagem rececbida: \n{}", message);
     }
 
-    @CustomConsumerListener(groupId = "group-1")
-    public void listenerTwoGroupOne(String message){
-        log.info("listenerTwoGroupOne ::: Nova mensagem rececbida: \n{}", message);
-    }
-
     @CustomConsumerListener(groupId = "group-2")
     public void listenerOneGroupTwo(String message){
         log.info("listenerOneGroupTwo ::: Nova mensagem rececbida: \n{}", message);
     }
 
-    @CustomConsumerListener(groupId = "group-2")
-    public void listenerTwoGroupTwo(String message){
-        log.info("listenerTwoGroupTwo ::: Nova mensagem rececbida: \n{}", message);
+    @KafkaListener(topics = { "main-topic" }, groupId = "group-3", containerFactory = "validMessageContainerFactory")
+    public void listenerComValidacao(String message){
+        log.info("listenerComValidacao ::: Nova mensagem rececbida: \n{}", message);
     }
 
 }
